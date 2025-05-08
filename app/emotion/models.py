019e0d7ed67d0acd2f19ff46a8bfb81a3442f89f
duehee <149302959+duehee@users.noreply.github.com>
2025-05-09 00:09:15 +0900

    chore : 감정 오분류 모델 수정

--- app/emotion/models.py
+++ app/emotion/models.py
@@ -1,6 +1,6 @@
 import torch
-import torch.nn as nn
-from transformers import BertModel, BertTokenizer, AutoTokenizer, AutoModel
+from transformers import BertForSequenceClassification
+from kobert_tokenizer import KoBERTTokenizer
 
 from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.orm import relationship
@@ -30,24 +30,9 @@ class EmotionType(Base):
     quadrant = Column(Integer, nullable=False)
     related_emotion_id = Column(Integer, ForeignKey("emotionType.id", ondelete="SET NULL"), nullable=True)
 
-    # ✅ 관계 명칭을 `diaries`로 변경
     diaries = relationship("Diary", back_populates="emotion")
 
-class EmotionClassifier(nn.Module):
-    def __init__(self, num_classes=8):
-        super(EmotionClassifier, self).__init__()
-        self.bert = AutoModel.from_pretrained("skt/kobert-base-v1")
-        self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_classes)
-
-    def forward(self, input_ids, attention_mask):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooled = outputs.pooler_output
-        return self.classifier(self.dropout(pooled))
-
-tokenizer = AutoTokenizer.from_pretrained("skt/kobert-base-v1")
-
-model = EmotionClassifier(num_classes=len(emotion_labels))
-state_dict = torch.load("app/emotion/best_model(1st).pt", map_location=torch.device("cpu"))
-model.load_state_dict(state_dict)
+tokenizer = KoBERTTokenizer.from_pretrained("skt/kobert-base-v1")
+model = BertForSequenceClassification.from_pretrained("skt/kobert-base-v1", num_labels=8)
+model.load_state_dict(torch.load("app/emotion/best_model(7th).pt", map_location="cpu"))
 model.eval()
